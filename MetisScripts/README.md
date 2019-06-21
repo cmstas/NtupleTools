1e65e040c41b18f6c60e8ed2cacaa44e1514a128
Nick Amin <amin.nj@gmail.com>
2019-06-20 20:48:13 -0700

    another script to check for things to delete

--- MetisScripts/README.md
+++ MetisScripts/README.md
@@ -13,6 +13,7 @@ Main submission script is `bigly.py` because it submits **bigly**. Of course rea
 * `misc/to_backup.py` -- has functions to print out directories containing samples we care about (i.e., latest CMS4 tags, not ones scheduled for deletion, etc)
 * `misc/find_corruptions.py` -- uses `misc/to_backup.py` and `hdfs fsck` to find corrupted files that we care to remake
 * `misc/find_older_cms4.py` -- example script to find outdated CMS4 according to DIS
+* `misc/find_older_cms4_general.py` -- similar to above, but just using `ls`, and uses python pandas if you're into that
 * `misc/get_local_commands.py` -- example script that prints out cmsRun commands to run CMS4 metis jobs locally
 * `misc/useful_condor_commands.sh` -- bash script with useful aliases/functions for condor
 * `misc/check_corrupt_fast.py` -- super fast (and super loose) check for corruptions in a folder with uproot
---
+++ MetisScripts/misc/find_older_cms4_general.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import commands
+import glob
+import pandas as pd
+from tqdm import tqdm
+
+def get_df(dirs):
+    """
+    Given list a hadoop directories, return physical directory sizes in GB
+    """
+    cmd = "hadoop fs -du -s {0}".format(" ".join([d.replace("/hadoop","") for d in dirs]))
+    stat,out = commands.getstatusoutput(cmd)
+    sizes = {}
+    for line in out.strip().splitlines():
+        try:
+            sizes["/hadoop"+line.split()[1]] = int(line.split()[0])*1.0e-9
+        except:
+            pass
+    data = []
+    # for path,size in tqdm(sizes.items()):
+    for path,size in sizes.items():
+        d = {}
+        d["size"] = size
+        d["path"] = path
+        d["campaign"] = d["path"].split("/group/snt/",1)[1].split("/",1)[0]
+        d["tag"] = d["path"].rsplit("_CMS4_",1)[-1]
+        d["sample"] = d["path"].split("/group/snt/",1)[1].split("/",1)[1].rsplit("_CMS4_",1)[0]
+        d["process"] = d["sample"].split("_RunII",1)[0]
+        # d["metadata"] = os.path.exists(d["path"]+"/metadata.json")
+        data.append(d)
+    df = pd.DataFrame(data)
+    df["size"] = df["size"].round(1)
+    return df
+
+if __name__ == "__main__":
+
+    df = pd.concat([
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_mc2016_94x/*")),
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_mc2017/*")),
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_mc2018/*")),
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_data2016_94x/*")),
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_data2017/*")),
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_data2018/*")),
+        get_df(glob.glob("/hadoop/cms/store/group/snt/run2_data2018/*")),
+        ]).reset_index(drop=True).drop_duplicates(["path"])
+    df.to_pickle("temp.pkl")
+
+    df = pd.read_pickle("temp.pkl")
+    groups = df.groupby(["sample"])
+    can_save = 0.
+    for name,group in groups:
+        if len(group) == 1: continue
+        group = group.sort_values("tag",ascending=False)
+        tags = list(group["tag"].values)
+        sizes = list(group["size"].values)
+        paths = list(group["path"].values)
+        for tag,size,path in zip(tags[1:],sizes[1:],paths[1:]):
+            print "{} # ({}GB), latest tag is {} and {}GB ({})".format(path,size,tags[0],sizes[0],paths[0])
+            can_save += size
+    print "# in total, can save {}GB by deleting old versions".format(can_save)
+
